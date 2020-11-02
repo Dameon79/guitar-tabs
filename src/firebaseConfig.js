ad8d55a17c9ec3bc655511c9d4fb7abbe9d5175e
Dameon Sumpton <dameon@Dameons-MacBook-Pro.local>
2020-11-02 08:06:40 +0000

    Add firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,6 @@
-import React from "react";
-import firebase from "firebase/app";
+import firebase from "firebase";
 
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyCYqjlJuJVFrik6jgQaRLEDdnOazqJqkak",
   authDomain: "guitar-tabs-95bf0.firebaseapp.com",
   databaseURL: "https://guitar-tabs-95bf0.firebaseio.com",
@@ -13,10 +9,13 @@ firebase.initializeApp({
   messagingSenderId: "609982359130",
   appId: "1:609982359130:web:7a1fe68613d842e49c1a63",
   measurementId: "G-EJXTMKHYR7",
-});
+};
 
-function App() {
-  return <div className="App">Alfie Smells</div>;
-}
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default App;
+const firebaseDb = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default firebaseDb;
